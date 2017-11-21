ad6e28462fd90796a4b01bc006a579e69fd95722
Joichiro Hashikawa <j.hashikawa.tech@gmail.com>
2017-11-21 12:55:06 +0900

    define validation and relation of post and template

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :outline, presence: true, length: { maximum: 50 }
+  validates :detail, presence: true, length: { maximum: 200 }
+  validates :private_flag, presence: true
+  validates :status, presence: true
+  validates :num_of_planner, presence: true
+  validates :num_of_engineer, presence: true
+  validates :num_of_designer, presence: true
+  validates :num_of_graphicer, presence: true
+
+  belongs_to :template
+
 end
--- app/models/template.rb
+++ app/models/template.rb
@@ -1,2 +1,7 @@
 class Template < ApplicationRecord
+
+  validates :image, presence: true
+
+  has_many :posts
+
 end
