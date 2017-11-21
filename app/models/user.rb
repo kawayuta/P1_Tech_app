c8e95450eff9b80e5bb032896211499a5da9f722
kawayuta <k216q013@stu.kyoto-seika.ac.jp>
2017-11-21 18:42:58 +0900

    add column education

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
-  def self.find_for_oauth(auth)
-    user = User.where(uid: auth.uid, provider: auth.provider).first
 
+  def self.find_for_oauth(auth)
+
+    user = User.where(uid: auth.uid, provider: auth.provider).first
     unless user
       user = User.create(
           uid:      auth.uid,
           provider: auth.provider,
-          email:    User.dummy_email(auth),
-          password: Devise.friendly_token[0, 20]
+          email:    auth.info.email,
+          password: Devise.friendly_token[0, 20],
+          education: auth.info.user_education_history.to_s
       )
+
     end
 
     user
