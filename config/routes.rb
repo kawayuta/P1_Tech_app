58dec1ed91909629073a675aaa8a3bcc50d62ac1
kawayuta <k216q013@stu.kyoto-seika.ac.jp>
2017-11-21 18:47:50 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  root to: 'top#index'
   resources :posts
   get '/top', to: 'top#index'
 
