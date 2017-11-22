bac39d7f6f0b6578ff36cc69ee7fa3036aa27e4b
kawayuta <k216q013@stu.kyoto-seika.ac.jp>
2017-11-23 01:30:01 +0900

    fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+images = [
+    'https://www.pakutaso.com/shared/img/page/top_os01.jpg',
+    'https://www.pakutaso.com/shared/img/page/top_os02.jpg',
+    'https://www.pakutaso.com/shared/img/page/top_os03.jpg',
+    'https://www.pakutaso.com/shared/img/page/top_os04.jpg',
+    'https://www.pakutaso.com/shared/img/page/top_os05.jpg',
+    'https://www.pakutaso.com/shared/img/page/top_os06.jpg'
+]
+images.each do |img|
+  Template.create(image: img) unless Template.find_by(image: img)
+end
