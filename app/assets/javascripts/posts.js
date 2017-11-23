7bdde87228b06421bdbc545edc0125f045bc06b3
kawayuta <k216q013@stu.kyoto-seika.ac.jp>
2017-11-24 04:36:57 +0900

    add animetion

---
+++ app/assets/javascripts/posts.js
@@ -0,0 +1,11 @@
+$(function() {
+    $('.posts-post').hover(function() {
+        var link = this.children[1].children[0].getAttribute('href');
+        var t = setTimeout(function() {
+            location.href = link
+        }, 5000);
+        $(this).data('timeout', t);
+    }, function() {
+        clearTimeout($(this).data('timeout'));
+    });
+});
