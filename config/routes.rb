f12fa5edb15b858ce23dd63db8a00f5e5e27d9ab
kawayuta <k216q013@stu.kyoto-seika.ac.jp>
2017-11-24 03:28:53 +0900

    fix style

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
 
   resources :users, only:[:index,:show]
-
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    get 'sign_up' => 'devise/registrations#new'
+    get 'logout' => 'devise/sessions#destory'
+  end
+
   root to: 'top#index'
   resources :posts do
     member do
