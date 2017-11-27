ebe4d8b97d347133860012dcf077c3185dbb3b8e
kawayuta <k216q013@stu.kyoto-seika.ac.jp>
2017-11-28 00:32:41 +0900

    devise error fix

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -4,14 +4,19 @@ class Vote < ApplicationRecord
 
 
   def self.vote_support(user,post)
-    Vote.create(user_id:user.id,support:true) unless @vote = Vote.find_by(user_id: user,post_id:post)
-    if @vote.support
-      @vote.update(support:false)
-      return false
+    if @vote = Vote.find_by(user_id: user,post_id:post)
+      if @vote.support
+        @vote.update(support:false)
+        return false
+      else
+        @vote.update(support:true)
+        return true
+      end
     else
-      @vote.update(support:true)
+      Vote.create(user_id:user.id,support:true)
       return true
     end
+
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
 
   resources :users, only:[:index,:show]
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    get 'sign_up' => 'devise/registrations#new'
+    get 'logout' => 'devise/sessions#destory'
+  end
 
   root to: 'top#index'
 
