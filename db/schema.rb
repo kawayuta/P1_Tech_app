2bd68dd9181259e722fff25fc2a81d9bcf79b2af
Joichiro Hashikawa <j.hashikawa.tech@gmail.com>
2017-11-27 19:51:24 +0900

    create GroupMessage model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171127044241) do
+ActiveRecord::Schema.define(version: 20171127104911) do
+
+  create_table "group_messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text "content"
+    t.integer "post_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
