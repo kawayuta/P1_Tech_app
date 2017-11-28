e9d743a8bb20ef18da54deb81b5872dae4e88429
kawayuta <k216q013@stu.kyoto-seika.ac.jp>
2017-11-28 19:05:10 +0900

    fix modal view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require rails-ujs
-//= require turbolinks
 //= require vender/masonry.pkgd.min.js
 //= require vender/remodal.min
+//= require turbolinks
 //= require_tree .
