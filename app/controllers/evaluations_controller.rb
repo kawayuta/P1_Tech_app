70a712836996cdfcf348d4ba686e386d29d2667a
Joichiro Hashikawa <j.hashikawa.tech@gmail.com>
2017-12-03 22:59:52 +0900

    creating evaluation system

--- app/controllers/evaluations_controller.rb
+++ app/controllers/evaluations_controller.rb
@@ -1,6 +1,8 @@
 class EvaluationsController < ApplicationController
 
   def new
+    @post = Post.find(params[:post_id])
+    @members = @post.team_members.reject{|m| m.user_id == current_user.id}
   end
 
   def create
