69a1887cb2ea3d6b6402fc57c112c57d57e1c8ff
Joichiro Hashikawa <j.hashikawa.tech@gmail.com>
2017-12-05 02:37:55 +0900

    fix to show evaluation in user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    set_evaluations
   end
 
   def edit
@@ -24,14 +25,19 @@ class UsersController < ApplicationController
     end
   end
 
+  private
+    def set_user
+      return redirect_to root_path flash[:notice] = "お探しのペライチユーザーは見つかりませんでした。" unless User.find_by(id: params[:id])
+      @user = User.find(params[:id])
+    end
 
-  def set_user
-    return redirect_to root_path flash[:notice] = "お探しのペライチユーザーは見つかりませんでした。" unless User.find_by(id: params[:id])
-    @user = User.find(params[:id])
-  end
+    def set_evaluations
+      evaluation_columns = [:first_evaluated_user, :second_evaluated_user, :third_evaluated_user, :fourth_evaluated_user, :fifth_evaluated_user]
+      @evaluations = Hash[*evaluation_columns.map { |column| [column.to_s, Evaluation.where(column => @user.id).count] }.flatten]
+    end
 
-  def user_params
-    params.require(:user).permit(:username, :email)
-  end
+    def user_params
+      params.require(:user).permit(:username, :email)
+    end
 
 end
