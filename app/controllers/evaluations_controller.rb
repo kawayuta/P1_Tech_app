d580e9aeab5715559ae68552572d2488becbbad5
Joichiro Hashikawa <j.hashikawa.tech@gmail.com>
2017-12-04 22:08:34 +0900

    fix evaluation system

--- app/controllers/evaluations_controller.rb
+++ app/controllers/evaluations_controller.rb
@@ -1,7 +1,10 @@
 class EvaluationsController < ApplicationController
 
   def new
-    set_post_and_members
+    @post = Post.find(params[:post_id])
+    team_members = @post.team_members.where(accepted: true).reject{|m| m.user_id == current_user.id}
+    @members = team_members.map{|m| User.find(m.user_id)}
+    @members_hash = Hash[@members.map{|m| [m.id, m.username]}].to_json
   end
 
   def create
@@ -9,19 +12,12 @@ class EvaluationsController < ApplicationController
     if @evaluation.save
       redirect_to root_path
     else
-      set_post_and_members
       flash[:notice] = "未選択項目があります"
-      render :new
+      redirect_to new_evaluation_path(params[:post_id])
     end
   end
 
   private
-    def set_post_and_members
-      @post = Post.find(params[:post_id])
-      team_members = @post.team_members.where(accepted: true).reject{|m| m.user_id == current_user.id}
-      @members = team_members.map{|m| User.find(m.user_id)}
-      @members_hash = Hash[@members.map{|m| [m.id, m.username]}].to_json
-    end
 
     def evaluation_params
       params.permit(:post_id, :first_evaluated_user, :second_evaluated_user, :third_evaluated_user, :fourth_evaluated_user, :fifth_evaluated_user).merge(user_id: current_user.id)
