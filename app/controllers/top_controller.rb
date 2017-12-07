2027b1dee85d2398f5fcc4a7e009b58b86bc5361
Joichiro Hashikawa <j.hashikawa.tech@gmail.com>
2017-12-07 21:48:35 +0900

    get ranked post in top page

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,7 @@
 class TopController < ApplicationController
   def index
     @posts = Post.where(published: true).order('created_at DESC')
+    ranked_post_ids = Vote.where(support: true).group(:post_id).order('count_post_id DESC').limit(5).count(:post_id).keys
+    @ranked_posts = ranked_post_ids.map { |id| Post.find_by(id: id) }
   end
 end
