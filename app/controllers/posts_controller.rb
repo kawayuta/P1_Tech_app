d719d8ea92fdcecf096c14dcceb15ff0f91b3a84
kawayuta <k216q013@stu.kyoto-seika.ac.jp>
2017-12-13 03:41:50 +0900

    form fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -186,6 +186,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :detail, :image, :image_2, :image_3, :category_name, :motivation, :period, :user_id, :published, :status, :num_of_planner, :num_of_engineer, :num_of_designer).merge(user_id: current_user.id)
+      params.require(:post).permit(:title, :detail, :image, :image_2, :image_3, :category_name, :motivation, :period, :main_color,:user_id, :published, :status, :num_of_planner, :num_of_engineer, :num_of_designer).merge(user_id: current_user.id)
     end
 end
