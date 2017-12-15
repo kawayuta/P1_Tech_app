5008c04370d142f7c2279823fc6c82a44afc9a7e
Joichiro Hashikawa <j.hashikawa.tech@gmail.com>
2017-12-15 17:11:38 +0900

    fix comment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def created_time_distance(instance)
+    distance = (Time.now - instance.created_at).to_i
+    if distance >= 604800
+      return "#{instance.created_at.strftime("%m/%d %H:%M")}"
+    elsif distance >= 86400
+      return "#{distance / 86400}日前"
+    elsif distance >= 3600
+      return "#{distance / 3600}時間前"
+    else
+      return "#{distance / 3600}分前"
+    end
+  end
+
 end
