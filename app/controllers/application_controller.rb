2ecb9ffc519bd36b12bc2e968690fad77498dc5e
Joichiro Hashikawa <j.hashikawa.tech@gmail.com>
2017-12-18 21:34:57 +0900

    get unread messages count

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,8 +29,18 @@ class ApplicationController < ActionController::Base
       joining_post_ids = current_user.team_members.where(accepted: true).map{|m| m.post_id}
       message_existing_post_ids = joining_post_ids.select{|id| GroupMessage.find_by(post_id: id) != nil}
       sorted_post_ids = message_existing_post_ids.sort_by{|id| GroupMessage.where(post_id: id).last.created_at}.reverse
-      @sorted_posts_and_messages = sorted_post_ids.map{|id| [Post.find(id), GroupMessage.where(post_id: id).last]}
+      @sorted_posts_and_messages = sorted_post_ids.map{|id| [Post.find(id), GroupMessage.where(post_id: id).last, unread_messages_count(id)]}
     end
+  end
+
+  def unread_messages_count(post_id)
+    message_read_time = current_user.message_read_times.find_by(post_id: post_id)
+    if message_read_time.nil?
+      return GroupMessage.where(post_id: post_id).count
+    end
+
+    last_read_at = message_read_time.last_read_at
+    return GroupMessage.where(post_id: post_id).where('created_at > ?', last_read_at).count
   end
 
 end
