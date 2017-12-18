ad6bdab1048b4f804bfaa6ea2e79d3214bdd262d
Joichiro Hashikawa <j.hashikawa.tech@gmail.com>
2017-12-18 18:11:58 +0900

    move js code of group message

---
+++ app/assets/javascripts/group_messages.js
@@ -0,0 +1,13 @@
+$(document).on('turbolinks:load', function() {
+  window.scrollTo(0, document.body.scrollHeight);
+  $('#group-message-textarea').focus(function() {
+    $(this).css('height', '60px');
+    $('#talk-room-wrapper').css('margin-bottom', '110px');
+    window.scrollBy(0, 24);
+  });
+  $('#group-message-textarea').blur(function() {
+    $(this).css('height', '24px');
+    window.scrollBy(0, -24);
+    $('#talk-room-wrapper').css('margin-bottom', '74px');
+  });
+});
