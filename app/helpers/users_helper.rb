fd004445ac4d29d67ee03d36fef61fb35c85f648
Joichiro Hashikawa <j.hashikawa.tech@gmail.com>
2017-12-19 19:14:01 +0900

    fix user prof header

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,25 @@
 module UsersHelper
+
+  def user_role(user)
+    case user.try(:role)
+    when 0
+      return 'Planner'
+    when 1
+      return 'Engineer'
+    when 2
+      return 'Designer'
+    end
+  end
+
+  def user_role_color(user)
+    case user.try(:role)
+    when 0
+      return '#45D480'
+    when 1
+      return '#F8BF4A'
+    when 2
+      return '#FE6B6B'
+    end
+  end
+
 end
