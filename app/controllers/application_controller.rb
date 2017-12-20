3d8ffed290af225cccd43e5ec756341c6395612e
Masaya Murakami <masa9928@icloud.com>
2017-12-21 02:12:59 +0900

    small fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    edit_user_path(current_user)
+    root_path
   end
 
   def set_new_messages
