910f59d152a93ae8c2fba30badd70e49059d13d6
Joichiro Hashikawa <j.hashikawa.tech@gmail.com>
2017-12-22 04:33:19 +0900

    fix users_helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,22 +2,22 @@ module UsersHelper
 
   def user_role(user)
     case user.try(:role)
-    when 0
-      return 'Planner'
     when 1
-      return 'Engineer'
+      return 'Planner'
     when 2
+      return 'Engineer'
+    when 3
       return 'Designer'
     end
   end
 
   def user_role_color(user)
     case user.try(:role)
-    when 0
-      return '#45D480'
     when 1
-      return '#F8BF4A'
+      return '#45D480'
     when 2
+      return '#F8BF4A'
+    when 3
       return '#FE6B6B'
     end
   end
