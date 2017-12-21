b4a52b024f2e4823979a56629a98b405e15ad5f5
Joichiro Hashikawa <j.hashikawa.tech@gmail.com>
2017-12-21 22:31:42 +0900

    fix group message

--- app/assets/javascripts/group_messages.js
+++ app/assets/javascripts/group_messages.js
@@ -1,13 +1,10 @@
 $(document).on('turbolinks:load', function() {
+
   $('#group-message-textarea').focus(function() {
     $(this).css('height', '60px');
-    $('#talk-room-wrapper').css('margin-bottom', '110px');
-    window.scrollBy(0, 24);
   });
   $('#group-message-textarea').blur(function() {
     $(this).css('height', '24px');
-    window.scrollBy(0, -24);
-    $('#talk-room-wrapper').css('margin-bottom', '74px');
   });
 
   $('#room-menu-open-btn').click(function() {
